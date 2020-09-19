5ceb1952837c1ac81fb322ddf89d4e8cd6597473
DimaVakarchuk <DeepVibes@yandex.ru>
2020-09-19 19:02:15 +0300

    Added 100% code coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,12 +9,18 @@ class StatisticsServiceTest {
   @Test
   void findMax() {
     StatisticsService service = new StatisticsService();
-
     long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
-
     long actual = service.findMax(incomesInBillions);
 
     assertEquals(expected, actual);
+  }
+
+@Test
+  void findMin() {
+    StatisticsService service = new StatisticsService();
+    long[] incomesInBillions = {15, 7, 8, 4, 5, 25, 8, 6, 17, 11, 15};
+    long expected = 25;
+    assertEquals(25, service.findMax(incomesInBillions));
   }
 }
